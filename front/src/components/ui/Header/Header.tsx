8c3eaa90ee416a3ac8ef89fef5bc85a35bfbb801
KOCNS1 <enes1.koc@outlook.com>
2022-11-15 15:46:10 +0100

    :lipstick: [CLEAN] Header component

--- front/src/components/ui/Header/Header.tsx
+++ front/src/components/ui/Header/Header.tsx
@@ -1,20 +1,36 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Disclosure } from "@headlessui/react";
+import { Link, useNavigate } from "react-router-dom";
 import { useStateContext } from "../../../context/auth/auth.context";
 import { logoutUserFn } from "../../../api/auth";
+import SearchBar from "../SearchBar";
+import BurgerMenu from "./responsiveScreens/BurgerMenu";
+import BurgerMenuContent from "./responsiveScreens/BurgerMenuContent";
+import LargeScreenMenuContent from "./largeScreen/LargeScreenMenuContent";
+import LargeScreenNavButtons from "./largeScreen/LargeScreenNavButtons";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
-const navigation = [
+export const navigation = [
   { name: "Home", href: "/" },
   { name: "My List", href: "/list" },
   { name: "Movies", href: "/movies" },
   { name: "TV Shows", href: "/tv" },
+];
+
+export const userMenu = [
+  {
+    name: "Your Profile",
+    to: "/profile",
+  },
+  {
+    name: "Settings",
+    to: "#",
+  },
+  {
+    name: "Sign out",
+    to: "#",
+    action: (handleLogout: () => void) => {
+      handleLogout();
+    },
+  },
 ];
 
 type Props = {
@@ -43,229 +59,18 @@ const Header = ({ setOpen }: Props) => {
                 <div className="flex-shrink-0 text-white font-bold">
                   <Link to={"/"}>TMDB</Link>
                 </div>
-                <div className="hidden lg:ml-6 lg:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <NavLink
-                        key={item.name}
-                        to={item.href}
-                        className={({ isActive, isPending }) =>
-                          classNames(
-                            isActive
-                              ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                            "px-3 py-2 rounded-md text-sm font-medium"
-                          )
-                        }
-                      >
-                        {item.name}
-                      </NavLink>
-                    ))}
-                  </div>
-                </div>
+                <LargeScreenNavButtons />
               </div>
-              <div className="flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end">
-                <div
-                  className="w-full max-w-lg lg:max-w-xs"
-                  onClick={() => setOpen(true)}
-                >
-                  <label htmlFor="search" className="sr-only">
-                    Search
-                  </label>
-                  <div className="relative">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                      <MagnifyingGlassIcon
-                        className="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <input
-                      id="search"
-                      name="search"
-                      className="block w-full rounded-md border border-transparent bg-gray-700 py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
-                      placeholder="Search"
-                      type="search"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex lg:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="hidden lg:ml-4 lg:block">
-                {!state.loggedIn ? (
-                  <NavLink
-                    to={"/auth"}
-                    className="rounded-md border border-transparent bg-blue-500 py-2 px-6 text-base font-medium text-white shadow-md hover:bg-blue-600"
-                  >
-                    Login
-                  </NavLink>
-                ) : (
-                  <UserProfile handleLogout={handleLogout} />
-                )}
-              </div>
+              <SearchBar setOpen={setOpen} />
+              <BurgerMenu open={open} />
+              <LargeScreenMenuContent handleLogout={handleLogout} />
             </div>
           </div>
 
-          <Disclosure.Panel className="lg:hidden">
-            {({ close }) => (
-              <>
-                <div className="space-y-1 px-2 pt-2 pb-3">
-                  {navigation.map((item) => (
-                    <NavLink
-                      to={item.href}
-                      key={item.name}
-                      className={({ isActive, isPending }) =>
-                        classNames(
-                          isActive
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "block px-3 py-2 rounded-md text-base font-medium"
-                        )
-                      }
-                      onClick={() => close()}
-                    >
-                      {item.name}
-                    </NavLink>
-                  ))}
-                </div>
-                <div className="border-t border-gray-700 pt-4 pb-3">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium text-white">
-                        Tom Cook small
-                      </div>
-                      <div className="text-sm font-medium text-gray-400">
-                        tom@example.com
-                      </div>
-                    </div>
-                    <button
-                      type="button"
-                      className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    <Disclosure.Button
-                      as="a"
-                      href="#"
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                    >
-                      Your Profile
-                    </Disclosure.Button>
-                    <Disclosure.Button
-                      as="a"
-                      href="#"
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                    >
-                      Settings
-                    </Disclosure.Button>
-                    <Disclosure.Button
-                      as="a"
-                      href="#"
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                    >
-                      Sign out
-                    </Disclosure.Button>
-                  </div>
-                </div>
-              </>
-            )}
-          </Disclosure.Panel>
+          <BurgerMenuContent handleLogout={handleLogout} />
         </>
       )}
     </Disclosure>
-  );
-};
-
-const userMenu = [
-  {
-    name: "Your Profile",
-    to: "/profile",
-  },
-  {
-    name: "Settings",
-    to: "#",
-  },
-  {
-    name: "Sign out",
-    to: "#",
-    action: (handleLogout: () => void) => {
-      handleLogout();
-    },
-  },
-];
-
-const UserProfile = ({ handleLogout }: { handleLogout: () => void }) => {
-  return (
-    <div className="flex items-center">
-      <button
-        type="button"
-        className="flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-      >
-        <span className="sr-only">View notifications</span>
-        <BellIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-
-      {/* Profile dropdown */}
-      <Menu as="div" className="relative ml-4 flex-shrink-0">
-        <div>
-          <Menu.Button className="flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-            <span className="sr-only">Open user menu</span>
-            <img
-              className="h-8 w-8 rounded-full"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            {userMenu.map((item) => (
-              <Menu.Item key={item.name}>
-                {({ active }) => (
-                  <Link
-                    to={item.to}
-                    className={classNames(
-                      active ? "bg-gray-100" : "",
-                      "block px-4 py-2 text-sm text-gray-700"
-                    )}
-                  >
-                    {item.name}
-                  </Link>
-                )}
-              </Menu.Item>
-            ))}
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </div>
   );
 };
 
